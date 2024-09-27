3dac0398d23f9e4701b1f5b25cc8c4f9372c0310
JesusLopezGomez <jelopez.gom@gmail.com>
2024-09-27 14:05:48 +0200

    preparando para el despliegue

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,1 +1,6 @@
-export const environment = {};
+export const environment = {
+    urlUser:"https://trello-api-vaw4.onrender.com/api/user",
+    urlProject:"https://trello-api-vaw4.onrender.com/api/project",
+    urlBoard:"https://trello-api-vaw4.onrender.com/api/board",
+    urlTask:"https://trello-api-vaw4.onrender.com/api/task"
+};
